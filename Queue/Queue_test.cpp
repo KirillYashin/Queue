c2708e44a760679056c9c3f00beafd72334eda1f
Kirill Yashin <34216147+KirillYashin@users.noreply.github.com>
2021-04-16 20:51:08 +0300

    Add files via upload

---
+++ Queue/Queue_test.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <locale>
+#include"Queue.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int len;
+	std::cout << "Введите длину очереди";
+	std::cout << std::endl;
+	std::cin >> len;
+	try
+	{
+		Queue<int> queue(len);
+		for (int i = 0; i < len; i++)
+		{
+			queue.Push(i);
+		}
+		std::cout << std::endl;
+		std::cout << "В очереди лежит:" << std::endl;
+		queue.Print();
+		std::cout << std::endl;
+		std::cout << "Выведем вершину очереди" << std::endl;
+		std::cout << queue.Top() << std::endl;
+		std::cout << std::endl;
+		std::cout << "Будем забирать элементы из очереди до тех пор, пока там что-то есть и выводить их на экран" << std::endl;
+		while (!queue.IsEmpty())
+		{
+			std::cout << "В очереди лежит " << queue.Get() << std::endl;
+		}
+		std::cout << std::endl;
+		std::cout << "Попробуем забрать еще элемент и получим исключение" << std::endl;
+		queue.Get();
+	}
+	catch (Exception exp)
+	{
+		exp.Print();
+	}
+	return 0;
+}
